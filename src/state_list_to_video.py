9938522643bdbcc2a2afdedbbc7b6c7812c9c2b2
jason <admin@itrustmachines.com>
2020-07-03 11:54:03 +0800

    add 6000000 result

--- src/state_list_to_video.py
+++ src/state_list_to_video.py
@@ -1,13 +1,18 @@
 from convert import images_to_video
 from convert import state_list_to_image
 
-_index = 0
+_index = 6000000
+_reward = 8925
+_steps = 35
 _fps = 10.0
 _resolution = (640, 640)
 _map_filepath = '../image/map.png'
 _output_dir = '../output/'
 
 
-def state_list_to_video(index, fps, resolution, map_filepath, output_dir):
-    state_list_to_image.state_list_to_image(index,,
-    images_to_video.images_to_video(index,)
+def state_list_to_video(index, reward, steps, fps, resolution, map_filepath, output_dir):
+    state_list_to_image.state_list_to_image(index, reward, steps, map_filepath, output_dir)
+    images_to_video.images_to_video(index, reward, steps, fps, resolution, output_dir)
+
+
+state_list_to_video(_index, _reward, _steps, _fps, _resolution, _map_filepath, _output_dir)
